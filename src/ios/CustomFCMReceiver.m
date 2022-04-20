1a1d0cb9853d71de04040e5c85f9f284e3b85ccf
sanaadostmuhammad <sanaadostmuhammad@gmail.com>
2022-04-20 22:54:29 +1000

    Sendbird payload stringified

--- src/ios/CustomFCMReceiver.m
+++ src/ios/CustomFCMReceiver.m
@@ -9,9 +9,22 @@ static NSString*const javascriptNamespace = @"cordova.plugin.customfcmreceiver";
     bool isHandled = false;
     if([userInfo objectForKey:@"sendbird"] != nil){
         isHandled = true;
-        [CustomFCMReceiverPlugin executeGlobalJavascript:[NSString stringWithFormat:@"%@._onMessageReceived(\"%@\")", javascriptNamespace, [self escapeDoubleQuotes:[userInfo objectForKey:@"sendbird"]]]];
+        [CustomFCMReceiverPlugin executeGlobalJavascript:[NSString stringWithFormat:@"%@._onMessageReceived(\"%@\")", javascriptNamespace, [self escapeDoubleQuotes:[self dictionaryToString:[userInfo objectForKey:@"sendbird"]]]]];
     }
     return isHandled;
+}
+
+- (NSString*)dictionaryToString:(NSDictionary*) sendbird {
+    NSError *error; 
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:sendbird 
+                                                    options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                        error:&error];
+    if (! jsonData) {
+        NSLog(@"Got an error: %@", error);
+    } else {
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        return jsonString;
+    }
 }
 
 - (NSString*)escapeDoubleQuotes: (NSString*)str {
